12f398a56927642b2820ce3488a2d22f585efce9
Jonas Eriksson <jouniicorn@gmail.com>
2020-09-08 12:56:06 +0200

    allt möjligt

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,50 +5,49 @@
  */
 
 module.exports = {
-  /* Your site config here */ plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.md`, `.mdx`],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    /* Your site config here */
+    plugins: [{
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 1200,
+                name: `pages`,
+                path: `${__dirname}/src/pages`,
             },
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
-        display: "swap",
-      },
-    },
-  ],
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `posts`,
+                path: `${__dirname}/src/posts`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-styled-component`,
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: [`.md`, `.mdx`],
+                gatsbyRemarkPlugins: [{
+                    resolve: `gatsby-remark-images`,
+                    options: {
+                        maxWidth: 1200,
+                    },
+                }, ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
+                display: "swap",
+            },
+        },
+    ],
 }
